3e119676a5c897b6f059b815b3405f5acb29e51f
qingshan.bai <xy926263@163.com>
2022-06-29 10:28:56 +0800

    demo

---
+++ src/main/java/com/it/spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.it.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/target")
+    public String target() {
+        return "target";
+    }
+}
